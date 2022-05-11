74f7e0080dd13ddb8af9dd052b3a426a32d7f0e7
Xzyra Lapid <83936947+xzyra14@users.noreply.github.com>
2022-05-11 23:18:47 +0800

    Add files via upload

---
+++ Activity06.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PDC03_MOD03
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Activity06 : ContentPage
+    {
+        public Activity06()
+        {
+            InitializeComponent();
+            {
+                this.BindingContext = this;
+                this.IsBusy = false;
+                this.btnLogin.Clicked += btnLogin_Clicked;
+            }
+
+            void btnLogin_Clicked(object sender, EventArgs e)
+            {
+                this.IsBusy = true;
+            }
+        }
+    }
+}
